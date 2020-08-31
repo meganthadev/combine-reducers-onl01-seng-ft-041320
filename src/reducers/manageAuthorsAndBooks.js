0040bdca6a54fc11a123ccb06958a39f7fe03972
Megan Horsfield <meganthadev@gmail.com>
2020-08-31 03:20:20 +0000

    Automatically backed up by Learn

--- src/reducers/manageAuthorsAndBooks.js
+++ src/reducers/manageAuthorsAndBooks.js
@@ -10,6 +10,7 @@ export default function bookApp(
     case "ADD_BOOK":
       return {
         ...state,
+        authors: [...state.authors],
         books: [...state.books, action.book]
       };
 
